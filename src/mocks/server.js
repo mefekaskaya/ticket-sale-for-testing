b000420e4406e63f798764ec85cbd64a97ad9d0d
mefekaskaya <mefekaskaya@gmail.com>
2023-05-03 22:14:11 +0300

    msw set up configured

---
+++ src/mocks/server.js
@@ -0,0 +1,6 @@
+import { setupServer } from "msw/node";
+
+import { handlers } from "./handlers";
+
+// This configures a requesting mocking server with the given request handlers
+export const server = setupServer(...handlers);
