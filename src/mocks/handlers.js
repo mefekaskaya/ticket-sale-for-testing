08306906fd7752f75ae66f7b6caccebab3fe4ad0
mefekaskaya <mefekaskaya@gmail.com>
2023-05-03 22:44:44 +0300

    handler added for band route with url params and band test added

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,10 +1,15 @@
 import { rest } from "msw";
 
+import { bandUrl } from "../features/band/redux/bandApi";
 import { showsUrl } from "../features/tickets/redux/showApi";
-import { shows } from "../test-utils/fake-data";
+import { bands, shows } from "../test-utils/fake-data";
 
 export const handlers = [
   rest.get(showsUrl, (req, res, ctx) => {
     return res(ctx.json({ shows }));
+  }),
+  rest.get(`${bandUrl}/:bandId`, (req, res, ctx) => {
+    const { bandId } = req.params;
+    return res(ctx.json({ band: bands[bandId] }));
   }),
 ];
